c59297bdf002e333571ed339025161686ab857ca
reirei62 <rei1gggg@gmail.com>
2020-08-22 18:21:38 +0900

    テストコード実行による修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :email,:last_name,:first_name,:kana_last_name, :kana_first_name,:password, presence: true
+  validates :nickname, :email,:last_name,:first_name,:kana_last_name, :kana_first_name,:password,:birth, presence: true
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/} 
   validates :kana_last_name, :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :password,length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
