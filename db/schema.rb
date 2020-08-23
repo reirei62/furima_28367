ceaabcb8d05a86f6235d37c14264c6a761047506
reirei62 <rei1gggg@gmail.com>
2020-08-24 03:51:34 +0900

    itemsコントローラーにnewアクション追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_162016) do
+ActiveRecord::Schema.define(version: 2020_08_23_164233) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,21 @@ ActiveRecord::Schema.define(version: 2020_08_23_162016) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "content", null: false
+    t.integer "category", null: false
+    t.integer "product_state", null: false
+    t.integer "delivery_fee", null: false
+    t.integer "shipper_area", null: false
+    t.integer "ship_days", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -52,4 +67,5 @@ ActiveRecord::Schema.define(version: 2020_08_23_162016) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
