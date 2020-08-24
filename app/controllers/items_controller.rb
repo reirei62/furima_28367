6605045c68ee959f7e5501c5c366b4466d94b16d
reirei62 <rei1gggg@gmail.com>
2020-08-24 16:30:23 +0900

    itemsコントローラーのcreateアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-  
+   Item.create(item_params)
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:price,:content,:category,:product_state,:delivery_fee,:shipper_area,:ship_days)
+  end
 end
