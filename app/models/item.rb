2c1754751c6cd0975758f403a4cd57a2df946e51
reirei62 <rei1gggg@gmail.com>
2020-08-25 01:38:38 +0900

    カテゴリー選択実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+
+  #空の投稿を保存できないようにする
+  validates :genre, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category, numericality: { other_than: 1 } 
+ 
 end
