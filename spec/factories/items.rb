1a5a932a0d3beec79cbf4b106bac279b3343ce3f
reirei62 <rei1gggg@gmail.com>
2020-08-27 16:30:19 +0900

    単体テストの大枠記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+      image                 {'output-image2.png'}
+      item_name             {Faker::Food.fruits}
+      price                 {Faker::Number.between(from:300,to:999999)}
+      content               {Faker::Lorem.sentence}
+      category_id           {Faker::Number.between(from:101,to:111)}
+      product_state_id      {Faker::Number.between(from:201,to:206)}
+      delivery_fee_id       {Faker::Number.between(from:301,to:302)}
+      shipper_area_id       {Faker::Number.between(from:1,to:47)}
+      ship_days_id          {Faker::Number.between(from:401,to:403)}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品がうまくいくとき' do
+    
+      it '全ての情報が記述されていれば出品できる' do
+        
+      end
+
+    end
+
+    
+    context '出品がうまくいかないとき' do
+     
+      it '画像が一枚なければ出品できない' do
+      end
+
+      it '商品の説明がなければ出品できない' do
+      end
+
+      it 'カテゴリーの情報がなければ出品できない' do
+      
+      end
+
+      it '商品の状態についての情報がなければ出品できない' do
+      end
+
+      it '配送料の負担についての情報がなければ出品できない' do
+      end
+
+      it '発送元の情報がなければ出品できない' do
+      end
+
+      it '発送までの日数についての情報がなければ出品できない' do
+      end
+
+      it '価格についての情報がなければ出品ができない' do
+      end
+
+      it '価格が300~9999999の範囲でなければ出品できない' do
+      end
+    end
+  end
 end
