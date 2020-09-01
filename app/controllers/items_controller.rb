f82b3745bf6009d652b2e848322dc0394a282c9f
reirei62 <rei1gggg@gmail.com>
2020-09-01 18:43:01 +0900

    destroyアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:update,:edit,:show]
-  before_action :move_to_index, only: [:update,:edit]
+  before_action :set_item, only: [:update,:edit,:show,:destroy]
+  before_action :move_to_index, only: [:update,:edit,:destroy]
 
   def index
     @items=Item.includes(:user).order("created_at DESC")
@@ -22,6 +22,11 @@ class ItemsController < ApplicationController
       render :edit
     end
     
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
   
   def show
