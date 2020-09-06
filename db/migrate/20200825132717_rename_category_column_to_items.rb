ba8c2513382eb87e3e677da19b8018a4a6a96d23
reirei62 <rei1gggg@gmail.com>
2020-09-06 22:38:37 +0900

    Update 20200825132717_rename_category_column_to_items.rb

--- db/migrate/20200825132717_rename_category_column_to_items.rb
+++ db/migrate/20200825132717_rename_category_column_to_items.rb
@@ -1,7 +1,6 @@
 class RenameCategoryColumnToItems < ActiveRecord::Migration[6.0]
   def change
     rename_column :items, :category, :category_id
-    rename_column :items, :product_state, :product_state_id
     rename_column :items, :delivery_fee, :delivery_fee_id
     rename_column :items, :shipper_area, :shipper_area_id
     rename_column :items, :ship_days,:ship_days_id
