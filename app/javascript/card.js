52c547b4ebf88fe014375b3a1751868361725368
reirei62 <rei1gggg@gmail.com>
2020-09-06 14:21:04 +0900

    Payjpの記述修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,11 +15,17 @@ const pay = () => {
     };
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
+        console.log("あ");
         const token = response.id;
+        console.log("い");
         const renderDom = document.getElementById("charge-form");
+        console.log("う");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        console.log("え");
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        console.log("お");
       }
+      console.log("か");
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
